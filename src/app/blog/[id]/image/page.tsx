faa81415760db3d2a8f149c74062160e70625c10
Harenabs21 <hei.harena.6@gmail.com>
2024-03-10 17:40:21 +0300

    feat: path /blog/[id]/image

---
+++ src/app/blog/[id]/image/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from '../../../page.module.css'
+import Image from 'next/image'
+import Header from '@/app/components/Header'
+import Footer from '@/app/components/Footer'
+import BlogList from '@/app/components/BlogList'
+import { notFound } from 'next/navigation'
+
+
+const style = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '2rem'
+}
+export default function page({ params }: { params: { id: number } }) {
+  const imageUrl = `https://picsum.photos/200/300?random=${params.id}`;  
+  return params.id < 1 || params.id > 10 ? notFound() : (
+    <main className={styles.main}>
+      <Header />
+      <div style={style}>
+        <BlogList />
+        <div>
+            <h2>Welcome to the blog number: {params.id}</h2>
+            <Image src={imageUrl} alt='' width={200} height={300} />
+        </div>  
+      </div>   
+      <Footer />
+    </main>
+  )
+}
